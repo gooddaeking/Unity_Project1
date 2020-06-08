44d3b4ccb11dcad0c6b7b9435752c09fd1dcee7b
gooddaeking <gooddaeking@gmail.com>
2020-06-08 13:59:53 +0900

    백그라운드 스크롤링
    플레이어 보스 에너미 모델교체

---
+++ Shooting Game/Assets/_JWH/Scripts/BackGround.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGround : MonoBehaviour
+{
+    Renderer bg;
+    float speed = 0.5f;
+    Vector2 xy = new Vector2(0, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bg = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //캐스팅해줘야한다
+        //xy.Set(0, xy.y + (speed * Time.deltaTime));
+        xy.y += Time.deltaTime * speed;
+        
+        bg.material.mainTextureOffset = xy;
+    }
+}
