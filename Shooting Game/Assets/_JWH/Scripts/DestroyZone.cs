a0b765a789b7f8a519ff65f8000a7b68925a0c57
gooddaeking <gooddaeking@gmail.com>
2020-06-09 16:00:42 +0900

    스크립트 오브젝트 풀링 수정

--- Shooting Game/Assets/_JWH/Scripts/DestroyZone.cs
+++ Shooting Game/Assets/_JWH/Scripts/DestroyZone.cs
@@ -8,9 +8,23 @@ public class DestroyZone : MonoBehaviour
     
     
     
-    private void OnTriggerEnter(Collider collider)
+    private void OnTriggerEnter(Collider other)
     {
         //이곳에서 트리거에 감지된 오브젝트 제거하기 ( 총알, 에너미)
-        Destroy(collider.gameObject);
+        //Destroy(collider.gameObject);
+
+        //if(other.gameObject.name.Contains("Bullet"))
+        //{
+        //    other.gameObject.SetActive(false);
+        //}
+
+        //레이어로 충돌체 찾기
+        if(other.gameObject.layer == LayerMask.NameToLayer("Bullet")
+        {
+            other.gameObject.SetActive(false);
+            //플레이어 오브젝트의 컴포넌트에 접근해서 처리한다
+            playerFire pf = GameObject.Find("Player").GetComponent<playerFire>();
+            pf.bulletPool.Add(other.gameObject);
+        }
     }
 }
--- Shooting Game/Assets/_JWH/Scripts/playerFire.cs
+++ Shooting Game/Assets/_JWH/Scripts/playerFire.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
@@ -9,7 +10,15 @@ public class playerFire : MonoBehaviour
     public GameObject firePoint;            //발사지점 좌표
 
     public AudioSource audio;
-    
+
+    //오브젝트 풀링
+    //오브젝트 풀링에 사용할 최대 총알개수
+    int poolSize = 20;
+    //1. 배열
+    //GameObject[] bulletPool;
+    //int fireIndex;
+    //2. 리스트
+    public List<GameObject> bulletPool;
 
     //레이저를 발사하기 위해서는 라인렌더러가 필요하다
     //선은 최소 2개의 점이 필요하다(시작점, 끝점)
@@ -27,17 +36,83 @@ public class playerFire : MonoBehaviour
         lr = GetComponent<LineRenderer>();
         //중요!!!
         //게임오브젝트는 활성화 비활성화 => SetActive() 함수 사용
+        
         //컴포넌트는 enabled 속성 사용
         audio = GetComponent<AudioSource>();
+
+        //오브젝트 풀링 초기화
+        InitObjectPooling();
+
+        //2. 리스트
+        bulletPool = new List<GameObject>();
+        for (int i = 0; i < poolSize; i++)
+        {
+
+        }
+    }
+
+    private void InitObjectPooling()
+    {
+        //1. 배열
+        //bulletPool = new GameObject[poolSize];
+        //for (int i = 0; i < poolSize; i++)
+        //{
+        //    GameObject bullet = Instantiate(bulletFactory);
+        //    bullet.SetActive(false);
+        //    bulletPool[i] = bullet;
+        //}
     }
 
     // Update is called once per frame
     void Update()
     {
+        Fire();
         FireRay();
         //레이저 보여주는 기능이 활성화되어있을 때만
         //레이져를 보여준다
         if (lr.enabled) ShowRay();
+    }
+
+    private void Fire()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            //1. 배열 오브젝트풀링으로 총알 발사
+            
+            //bulletPool[fireIndex].SetActive(true);
+            //bulletPool[fireIndex].transform.position = firePoint.transform.position;
+            //bulletPool[fireIndex].transform.up = firePoint.transform.up;
+            //fireIndex++;
+            //if(fireIndex >= poolSize)
+            //{
+            //    fireIndex = 0;
+            //}
+
+            //2. 리스트 오브젝트 풀링으로 총알발사 ( 진짜 오브젝트 풀링)
+            if(bulletPool.Count >0)
+            {
+                GameObject bullet = bulletPool[0];
+                bullet.SetActive(true);
+                bullet.transform.position = firePoint.transform.position;
+                bullet.transform.up = firePoint.transform.up;
+                //오브젝트 풀에서 빼준다
+                bulletPool.Remove(bullet);
+            }
+            else // 남은 총알이 없다, 고로 Instanciate으로 생성해준다
+            {
+                GameObject bullet = Instantiate(bulletFactory);
+                bullet.SetActive(false);
+                bulletPool.Add(bullet);
+            }
+
+            //총알공장(총알프리팹)에서 총알을 무한대로 찍어낼 수 있다
+            //Instantiate() 함수로 프리팹 파일을 게임오브젝트로 만든다
+
+            //총알 게임오브젝트 생성
+            //GameObject bullet = Instantiate(bulletFactory);
+            //총알 오브젝트의 위치 지정
+            //bullet.transform.position = firePoint.transform.position;
+        }
     }
 
     private void ShowRay()
